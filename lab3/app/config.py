0e2a8a3fa396a96c41e936e2091242d91a03f1f0
User <rustv2002@mail.ru>
2024-05-04 09:55:02 +0300

    update configs

--- lab3/app/config.py
+++ lab3/app/config.py
@@ -1,3 +1,4 @@
 import os
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'ddfb56d89e77814c4b57b6e2fe2cde6ca6d758afc4d68561939899415cb6e92b'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
--- lab4/app/config.py
+++ lab4/app/config.py
@@ -1,8 +1,14 @@
 import os
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'bc4c54c7519d1619a97d210b40fe519556efc7714ea61a1d369758bc9468b771'
+MYSQL_USER = 'std_2443_lab4'
+MYSQL_PASSWORD = '9257102455'
+MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
+MYSQL_DATABASE = 'std_2443_lab4'
 
-MYSQL_USER = os.environ.get('MYSQL_USER')
-MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE')
-MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
-MYSQL_HOST = os.environ.get('MYSQL_HOST')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# MYSQL_USER = os.environ.get('MYSQL_USER')
+# MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE')
+# MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
+# MYSQL_HOST = os.environ.get('MYSQL_HOST')
